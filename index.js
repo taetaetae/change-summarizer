d3bb4186190356e0a18f48926482ac847508bfd5
taetaetae <taetaetae_@naver.com>
2023-06-22 00:52:31 +0900

    param

--- index.js
+++ index.js
@@ -6,14 +6,12 @@ try {
     const compare = core.getInput('compare')
     const token = core.getInput('token')
 
-    console.log(`base : ${base}, compare : ${compare}`)
+    console.log(`base : ${base}, compare : ${compare}, token : ${token}`)
 
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
     const octokit = github.getOctokit(token)
     const repository = github.context.payload.repository
     const ownerName = repository.owner.name
     const repositoryName = repository.name;
-
     const request = octokit.request(`GET /repos/${ownerName}/${repositoryName}/compare/${base}...${compare}`);
 
     const result = JSON.stringify(request, undefined, 2);
