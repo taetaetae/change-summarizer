c9b4894456a9d36af41713e4ac98d6fc26cb3798
taetaetae <taetaetae_@naver.com>
2023-06-22 01:50:46 +0900

    param

--- index.js
+++ index.js
@@ -15,24 +15,41 @@ async function main() {
     const response = await octokit.request(`GET /repos/${ownerName}/${repositoryName}/compare/${base}...${compare}`)
 
     let pullRequests = new Map()
-    response.data.commits.forEach(function (commit) {
-        octokit.request(`GET /repos/${ownerName}/${repositoryName}/commits/${commit.sha}/pulls`)
-            .then(response => {
-                response.data.forEach(function (pull) {
-                    console.log(`number : ${pull.number}, title : ${pull.title}, user.login : ${pull.user.login}`)
-                    pullRequests.set(pull.number, {
-                        'number': `${pull.number}`,
-                        'title': `${pull.title}`,
-                        'login': `${pull.user.login}`
-                    })
-                })
-            })
-        console.log('-----')
-        pullRequests.forEach(function(data){
-            console.log(data)
-        })
-    })
+    // response.data.commits.forEach(function (commit) {
+    //     octokit.request(`GET /repos/${ownerName}/${repositoryName}/commits/${commit.sha}/pulls`)
+    //         .then(response => {
+    //             response.data.forEach(function (pull) {
+    //                 console.log(`number : ${pull.number}, title : ${pull.title}, user.login : ${pull.user.login}`)
+    //                 pullRequests.set(pull.number, {
+    //                     'number': `${pull.number}`,
+    //                     'title': `${pull.title}`,
+    //                     'login': `${pull.user.login}`
+    //                 })
+    //             })
+    //         })
+    // })
 
+    octokit.request(`GET /repos/${ownerName}/${repositoryName}/compare/${base}...${compare}`)
+        .then(response => {
+            response.data.commits.forEach(function (commit) {
+                octokit.request(`GET /repos/${ownerName}/${repositoryName}/commits/${commit.sha}/pulls`)
+                    .then(response => {
+                        response.data.forEach(function (pull) {
+                            console.log(`number : ${pull.number}, title : ${pull.title}, user.login : ${pull.user.login}`)
+                            pullRequests.set(pull.number, {
+                                'number': `${pull.number}`,
+                                'title': `${pull.title}`,
+                                'login': `${pull.user.login}`
+                            })
+                        })
+                    })
+            })
+        })
+
+    console.log('-----')
+    pullRequests.forEach(function (data) {
+        console.log(data)
+    })
 
 
 }
