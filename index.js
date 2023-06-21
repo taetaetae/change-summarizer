2a4c9fb829ddf7ef9460436812e303fe5d495286
taetaetae <taetaetae_@naver.com>
2023-06-22 00:31:41 +0900

    param

--- index.js
+++ index.js
@@ -2,12 +2,26 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-    const base = core.getInput('base');
-    const compare = core.getInput('compare');
+    const base = core.getInput('base')
+    const compare = core.getInput('compare')
+    const token = core.getInput('token')
+
     console.log(`base : ${base}, compare : ${compare}`)
 
     const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    const octokit = github.getOctokit(token)
+    let repository = github.context.payload.repository;
+
+    let compareCommits = octokit.rest.compareCommits({
+        repository.owner.name,
+        repository.name,
+        base,
+        compare,
+    });
+
+    const result = JSON.stringify(compareCommits, undefined, 2);
+    console.log(`result : ${result}`)
+
 } catch (error) {
     core.setFailed(error.message);
 }
