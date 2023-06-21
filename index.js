ebc5971555b68ebca69fdf6ea2f6149bcc5a9bc5
taetaetae <taetaetae_@naver.com>
2023-06-22 02:04:28 +0900

    param

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-const base = core.getInput('base')
-const compare = core.getInput('compare')
-const token = core.getInput('token')
+async function main() {
+    const base = core.getInput('base')
+    const compare = core.getInput('compare')
+    const token = core.getInput('token')
 
-console.log(`base : ${base}, compare : ${compare}, token : ${token}`)
+    console.log(`base : ${base}, compare : ${compare}, token : ${token}`)
 
-const octokit = github.getOctokit(token)
-const repository = github.context.payload.repository
-const ownerName = repository.owner.name
-const repositoryName = repository.name
-var pullRequests = new Map()
-octokit.request(`GET /repos/${ownerName}/${repositoryName}/compare/${base}...${compare}`)
-    .then(response => {
-        response.data.commits.forEach(function (commit) {
-            octokit.request(`GET /repos/${ownerName}/${repositoryName}/commits/${commit.sha}/pulls`)
-                .then(response => {
-                    response.data.forEach(function (pull) {
-                        console.log(`number : ${pull.number}, title : ${pull.title}, user.login : ${pull.user.login}`)
-                        pullRequests.set(pull.number, {
-                            'number': `${pull.number}`,
-                            'title': `${pull.title}`,
-                            'login': `${pull.user.login}`
+    const octokit = github.getOctokit(token)
+    const repository = github.context.payload.repository
+    const ownerName = repository.owner.name
+    const repositoryName = repository.name
+    const pullRequests = new Map()
+    await octokit.request(`GET /repos/${ownerName}/${repositoryName}/compare/${base}...${compare}`)
+        .then(response => {
+             response.data.commits.forEach(function (commit) {
+                octokit.request(`GET /repos/${ownerName}/${repositoryName}/commits/${commit.sha}/pulls`)
+                    .then(response => {
+                        response.data.forEach(function (pull) {
+                            console.log(`number : ${pull.number}, title : ${pull.title}, user.login : ${pull.user.login}`)
+                            pullRequests.set(pull.number, {
+                                'number': `${pull.number}`,
+                                'title': `${pull.title}`,
+                                'login': `${pull.user.login}`
+                            })
                         })
                     })
-                    console.log('---1--')
-                    console.log(pullRequests)
-                })
+            })
         })
-    })
+
+
+    console.log('---1--')
+    console.log(pullRequests)
+}
+
+main()
