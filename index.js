5fb75b4fe36f67df01d8bd167ddb521a47daa10b
taetaetae <taetaetae_@naver.com>
2023-06-21 23:23:39 +0900

    test

---
+++ index.js
@@ -0,0 +1,13 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+    const token = core.getInput('token');
+    console.log(`token : ${token}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+} catch (error) {
+    core.setFailed(error.message);
+}
